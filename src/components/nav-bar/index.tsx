aeeafea98b1e03807c908daf8b2a54d60a0af685
Mikhail Elanov <Elanov1991@Gmail.com>
2024-06-12 13:29:22 +0700

    Add some fix and new file

---
+++ src/components/nav-bar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <nav>
+        <ul className="flex flex-col gap-5">
+            <li>
+                <NavButt
+            </li>
+        </ul>
+    </nav>
+  )
+}
