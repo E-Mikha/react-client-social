e22d000a279b79daa822d0e1672b36d4d11aef11
Mikhail Elanov <Elanov1991@Gmail.com>
2024-06-17 09:18:19 +0700

    Changes layout, add selected from userSlice

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,25 @@
-import React from "react"
-import { Outlet } from "react-router-dom"
+import React, { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
 import { Header } from "../header"
 import { Container } from "../container"
 import { NavBar } from "../nav-bar"
+import { useSelector } from "react-redux"
+import {
+  selectIsAuthenticated,
+  selectUser,
+} from "../../features/user/userSlice"
 
 export const Layout = () => {
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+  const user = useSelector(selectUser)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/auth")
+    }
+  }, [])
+
   return (
     <>
       <Header />
