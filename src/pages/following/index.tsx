ae1f91edd2b041b02cf177d0ed9228cc094354d5
Mikhail Elanov <Elanov1991@Gmail.com>
2024-06-21 09:18:45 +0700

    Made Following component

--- src/pages/following/index.tsx
+++ src/pages/following/index.tsx
@@ -1,5 +1,34 @@
+import { Card, CardBody } from "@nextui-org/react"
 import React from "react"
+import { Link } from "react-router-dom"
+import { User } from "../../components/user"
+import { useSelector } from "react-redux"
+import { selectCurrent } from "../../features/user/userSlice"
 
 export const Following = () => {
-  return <div>Following</div>
+  const currentUser = useSelector(selectCurrent)
+
+  if (!currentUser) {
+    return null
+  }
+
+  return currentUser.following.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.following.map(user => (
+        <Link to={`/users/${user.following.id}`} key={user.following.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.following.name ?? ""}
+                avatarUrl={user.following.avatarUrl ?? ""}
+                description={user.following.email ?? ""}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <h2>У вас нет подписчиков</h2>
+  )
 }
